4f7fd5cd76268022e95ed0e1827a0420fe4ca126
Anurag Sedai <Anurag Sedai@DESKTOP-5ADAKG7>
2023-08-29 21:09:43 +0100

    Add project files.

---
+++ game trial 3/UserControls/Login.xaml.cs
@@ -0,0 +1,79 @@
+﻿using game_trial_3.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace game_trial_3.UserControls
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : UserControl
+    {
+        private readonly MediaPlayer mMediaPlayer = new MediaPlayer();
+
+        public Login()
+        {
+            InitializeComponent();
+            checkBoxMusic.IsChecked = (bool)Settings.Default["music"];
+            checkBoxMusic.Checked += CheckBoxMusic_Checked;
+            checkBoxMusic.Unchecked += CheckBoxMusic_Unchecked;
+            mMediaPlayer.Open(new Uri(string.Format("C:\\Users\\Anurag Sedai\\Documents\\Codin\\C#\\game trial 3\\game trial 3\\Music\\106TheRoadToVeridianFromPallet.wav", AppDomain.CurrentDomain.BaseDirectory)));
+            mMediaPlayer.MediaEnded += new EventHandler(Media_Ended);
+
+
+
+            if ((bool)Settings.Default["music"] == false)
+            {
+                mMediaPlayer.Stop();
+            }
+            else
+            {
+                mMediaPlayer.Position = TimeSpan.Zero;
+                mMediaPlayer.Play();
+            }
+        }
+
+        private void CheckBoxMusic_Checked(object sender, RoutedEventArgs e)
+        {
+            mMediaPlayer.Play();
+        }
+
+        private void CheckBoxMusic_Unchecked(object sender, RoutedEventArgs e)
+        {
+
+            mMediaPlayer.Stop();
+        }
+
+        private void Media_Ended(object sender, EventArgs e)
+        {
+            if ((bool)Settings.Default["music"] == true)
+            {
+                mMediaPlayer.Position = TimeSpan.Zero;
+                mMediaPlayer.Play();
+            }
+        }
+
+        private void btnLoginClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("I wnat your bobs");
+
+        }
+
+        private void btnCloseTab(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ game trial 3/Windows/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using game_trial_3.Properties;
+using game_trial_3.UserControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace game_trial_3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, IDisposable
+    {
+        void IDisposable.Dispose() { }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            if ((bool)Settings.Default["windowed"] == true)
+            {
+                WindowStyle = WindowStyle.ToolWindow;
+            }
+
+            WindowState = WindowState.Maximized;
+            Content = new MainMenu();
+        }
+    }
+}
