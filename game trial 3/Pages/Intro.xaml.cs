4bea51d438e31107efae8230fd87e6305ca2bea8
FlippinRag <anuragjedi@gmail.com>
2023-09-18 00:22:42 +0100

    OKAY i added: the lines being printed LETTER BY LETTER, it being checked all the damn time to make sure it doesnt BREAK and then FINALLY i made it so it made its own buttons (after being checked AND 100% SUREEEE its all finished speaking) for the pokemon that will have their PURPOSE IN LIFE!!!!!!! time to sleep and hand in whatever i have for draft 1

--- game trial 3/Pages/Intro.xaml.cs
+++ game trial 3/Pages/Intro.xaml.cs
@@ -1,45 +1,142 @@
 using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
 
 namespace game_trial_3.Pages
 {
     public partial class Intro : Page, IDisposable
     {
         void IDisposable.Dispose() { }
-        private readonly Random _random = new Random();
+        private Random random = new Random();
+        private List<string> Lines = new List<string>
+        {
+            "I think so...",
+            "I think it's time...",
+            "WELCOME TO THE POKEMON BATTLE SIMULATOR",
+            "My name is Professor Samuel Oak",
+            "Today I declare you prepared for the Battle world! :D",
+            "Its not easy mind you.. BUT I THINK YOU'RE READY!!!",
+            "The last step is to CHOOSE YOUR POKEMON",
+            
+        };
+
+        private int currentLinesIndex = 0;
+        private int currentSpeechIndex = 0;
+        private bool isLinesDone = false;
+        private DispatcherTimer textAnimationTimer;
+        private bool isSpeechDone = false;
+        private bool speechAnimationStarted = false;
         
         public Intro()
         {
             InitializeComponent();
             MovingAnimation();
+            textAnimationTimer = new DispatcherTimer();
+            textAnimationTimer.Interval = TimeSpan.FromMilliseconds(10); // can change how fast you want it to read the text
+            textAnimationTimer.Tick += TextAnimationTick;
+            StartSpeech();
             
-            /*GameScreen.Focus();
+        }
+        private void TextAnimationTick(object sender, EventArgs e)
+        {
+            if (currentLinesIndex < Lines.Count)
+            {
+                if (!isLinesDone)
+                {
+                    SpeechText.Text = ""; // Clear current line
+                    isLinesDone = true;
+                }
+                string currentSentence = Lines[currentLinesIndex];
+                if (currentSpeechIndex < currentSentence.Length)
+                {
+                    SpeechText.Text += currentSentence[currentSpeechIndex];
+                    currentSpeechIndex++;
+                }
+                else
+                {
+                    isLinesDone = false;
+                    currentLinesIndex++;
+                    currentSpeechIndex = 0;
+                    StartSpeech();
+                }
+            }
+            else
+            {
+                textAnimationTimer.Stop();
+                isSpeechDone = true;
 
-            GameTimer.Interval = TimeSpan.FromMilliseconds(16);
-            GameTimer.Tick += GameTick;
-            GameTimer.Start();*/
+            }
+        }
+    
+        private void CreatePokemonButtons() // creates buttons for the pokemon and assigns them a place in the stack panel
+        {
+            Button bulbasaurButton = new Button();
+            Image bulbasaurImage = new Image
+            {
+                Source = new BitmapImage(new Uri("pack://application:,,,/Images/bulbasaur.png", UriKind.RelativeOrAbsolute)),
+            };
+            bulbasaurButton.Content = bulbasaurImage;
+            bulbasaurButton.Width = bulbasaurImage.Source.Width;
+            bulbasaurButton.Height = bulbasaurImage.Source.Height;
+            bulbasaurButton.Click += PokemonSelected;
+
+            Button charmanderButton = new Button();
+            Image charmanderImage = new Image
+            {
+                Source = new BitmapImage(new Uri("pack://application:,,,/Images/charmander.png", UriKind.RelativeOrAbsolute))
+            };
+            charmanderButton.Content = charmanderImage;
+            charmanderButton.Width = charmanderImage.Source.Width;
+            charmanderButton.Height = charmanderImage.Source.Height;
+            charmanderButton.Click += PokemonSelected;
+            
+            ChooseYourPokemon.Children.Add(bulbasaurButton);
+            ChooseYourPokemon.Children.Add(charmanderButton);
+            
+            
+        }
+
+        private void PokemonSelected(object sender, RoutedEventArgs e) // place holder for actually using it
+        {
+            MessageBox.Show("nice");
+        }
+
+        private void StartSpeech() 
+        {
+            textAnimationTimer.Start();
+            MovingAnimation();
+            
+            if (!speechAnimationStarted) //checks if the speech started or not
+            {
+                textAnimationTimer.Tick += (sender, e) =>
+                {
+                    if (currentLinesIndex >= Lines.Count) // once speech has ended the buttons can now be made!!
+                    {
+                        textAnimationTimer.Stop();
+                        CreatePokemonButtons();
+                    }
+                };
+
+                speechAnimationStarted = true;
+            }
+            
         }
         
-        private void MovingAnimation()
+        private void MovingAnimation() // adds oak moving to make it look cool
         {
             DoubleAnimation animation = new DoubleAnimation
             {
                 From = Canvas.GetTop(Player),
-                Duration = TimeSpan.FromSeconds(0.2),
-                AutoReverse = true, // Make it jittery by moving up and down
-                RepeatBehavior = new RepeatBehavior(10), // Repeat 3 times, adjust as needed
+                Duration = TimeSpan.FromSeconds(0.25),
+                AutoReverse = true, 
+                RepeatBehavior = new RepeatBehavior(4), // Choose repetitions.. yea that's it
             };
 
-            animation.Completed += (sender, e) =>
-            {
-                // Reset the position when the animation is done
-                Canvas.SetTop(Player, 100);
-            };
-
-            // Set new target positions for jittery movement
-            double targetTop = Canvas.GetTop(Player) + _random.Next(-300, 301); // This is where you can set how far you want them to move
+            double targetTop = Canvas.GetTop(Player) + random.Next(1, 101); // Setting how far you can move them
 
             animation.To = targetTop;
 
@@ -48,7 +145,7 @@ namespace game_trial_3.Pages
 
             Storyboard storyboard = new Storyboard();
             storyboard.Children.Add(animation);
-            storyboard.Begin(this);
+            storyboard.Begin(Player);
         }
 
         
